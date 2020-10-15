528f3e2efe4796e0c94d1bb0838109030bb06a5b
ie715536 <ie715536@iteso.mx>
2020-10-14 21:35:01 -0500

    BMI160 init

---
+++ BMI160.c
@@ -0,0 +1,8 @@
+/*
+ * BMI160.c
+ *
+ *  Created on: 14 oct. 2020
+ *      Author: urik_
+ */
+
+
---
+++ BMI160.h
@@ -0,0 +1,13 @@
+/*
+ * BMI160.h
+ *
+ *  Created on: 14 oct. 2020
+ *      Author: urik_
+ */
+
+#ifndef BMI160_H_
+#define BMI160_H_
+
+
+
+#endif /* BMI160_H_ */
